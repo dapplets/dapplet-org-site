5f38d478d1728d7117350210642c4d2d65c6b0df
Deokti <reilgelern@mail.ru>
2021-09-22 16:15:16 +0300

    fix: changed pictures, changed buttons on the first screen, added floating button

--- src/assets/js/imports/augment-image.js
+++ src/assets/js/imports/augment-image.js
@@ -1,18 +1,27 @@
-const augment = document.querySelector('*[data-augment]');
+(function () {
+  const augment = document.querySelector('*[data-augment]');
 
-augment.addEventListener('click', (event) => {
-  const isChecked = event.target.checked;
-  const images = document.querySelectorAll('*[data-augment-image]');
+  augment && augment.addEventListener('click', (event) => {
+    const isChecked = event.target.checked;
+    const images = document.querySelectorAll('*[data-augment-image]');
 
-  if (isChecked) {
-    return images.forEach((item) => replacePicture(item, true));
+    if (isChecked) {
+      replaceText('Stop Augmenting!');
+      return images.forEach((item) => replacePicture(item, true));
+    }
+
+    replaceText('Augment Us!');
+    images.forEach((item) => replacePicture(item, false));
+  });
+
+  function replaceText(text) {
+    const augmentSpan = augment.querySelector('span');
+    augmentSpan.textContent = text;
   }
 
-  images.forEach((item) => replacePicture(item, false));
-});
-
-function replacePicture(image, state) {
-  return state === true
-    ? image.classList.add('augment-image')
-    : image.classList.remove('augment-image')
-}
+  function replacePicture(image, state) {
+    return state === true
+      ? image.classList.add('augment-image')
+      : image.classList.remove('augment-image')
+  }
+}())

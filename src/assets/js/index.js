b78801b662dccb8073fc047e6067ab59911ac0bb
Deokti <reilgelern@mail.ru>
2021-09-24 11:20:49 +0300

    fix: all repetitive code elements were moved to a separate script, a separate js file was created for each page, the augment button paddings were changed

---
+++ src/assets/js/index.js
@@ -0,0 +1,4 @@
+import './imports/augment-image';
+import './imports/behavior-scroll';
+import './imports/cookies';
+import './imports/height-first-screen';
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,3 @@
+// What is used on all pages
 import './imports/copywriting-time';
-import './imports/augment-image';
-import './imports/behavior-scroll';
-import './imports/cookies';
-import './imports/height-first-screen';
-
+import './imports/mailchimp-subscription';

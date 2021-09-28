f2812a6b91e7fa7e9a07a3a781a1efe28ebe6d66
Deokti <reilgelern@mail.ru>
2021-09-28 19:27:26 +0300

    fix: redesigned the code for smooth scrolling for several elements on the page

--- src/assets/js/imports/behavior-scroll.js
+++ src/assets/js/imports/behavior-scroll.js
@@ -1,11 +1,18 @@
-const buttonLink = document.querySelector('*[data-click=scroll]');
+const buttonLink = document.querySelectorAll('*[data-click=scroll]');
 
-buttonLink.addEventListener('click', function (event) {
+buttonLink.forEach(onClick)
+
+function onClick(button) {
+  button.addEventListener('click', behaviorScroll);
+}
+
+function behaviorScroll(event) {
   event.preventDefault();
-  const id = this.getAttribute('href').substr(1)
 
+  const id = this.getAttribute('href').substr(1);
   document.getElementById(id).scrollIntoView({
     behavior: 'smooth',
     block: 'start'
-  })
-});
+  });
+}
+

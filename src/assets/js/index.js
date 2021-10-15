0e1ab5032f585c70dbed751816d362b76d598ffe
Alexander Sakhaev <alsakhaev@gmail.com>
2021-10-15 21:57:27 +0300

    fix: removed cookies and counters

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,6 @@
 import './imports/copywriting-time';
 import './imports/augment-image';
 import './imports/behavior-scroll';
-import './imports/cookies';
+// import './imports/cookies';
 import './imports/height-first-screen';
 import './imports/augment-hide';

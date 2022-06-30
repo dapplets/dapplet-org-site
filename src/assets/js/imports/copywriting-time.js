c10f804e979545ceb6001d0de81d190246b9767e
Deokti <deokneks@gmail.com>
2022-06-30 09:41:24 +0300

    feat: mobile version /promo.html

--- src/assets/js/imports/copywriting-time.js
+++ src/assets/js/imports/copywriting-time.js
@@ -1,8 +1,10 @@
 function currentCopywritingTime() {
-  const element = document.querySelector('.footer__copywriting');
+  const element = document.querySelector(".footer__copywriting");
+  if (!element) return false;
+  console.log(element);
   const baseString = (time) => `© 2019—${time} Dapplets Project`;
 
   element.textContent = baseString(new Date().getFullYear());
 }
 
-document.addEventListener('DOMContentLoaded', currentCopywritingTime);
+document.addEventListener("DOMContentLoaded", currentCopywritingTime);

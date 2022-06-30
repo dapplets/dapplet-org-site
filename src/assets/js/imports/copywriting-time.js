85b0138aed2730ff05809b9dce274972e3a70fac
Deokti <deokneks@gmail.com>
2022-06-30 10:01:09 +0300

    fix: piwik inserted

--- src/assets/js/imports/copywriting-time.js
+++ src/assets/js/imports/copywriting-time.js
@@ -1,7 +1,6 @@
 function currentCopywritingTime() {
   const element = document.querySelector(".footer__copywriting");
   if (!element) return false;
-  console.log(element);
   const baseString = (time) => `© 2019—${time} Dapplets Project`;
 
   element.textContent = baseString(new Date().getFullYear());
